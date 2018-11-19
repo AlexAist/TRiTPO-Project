de088086c08a43c64eae43f72e61f65828995c4f
Alexey Buslov <alex_aist@mail.ru>
2018-11-20 02:58:04 +0300

    Create GUI

---
+++ Source/app/src/main/java/com/example/alex_/project/AddCarActivity.java
@@ -0,0 +1,13 @@
+package com.example.alex_.project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddCarActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_addcar);
+    }
+}
