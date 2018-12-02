acadf4b652415c9d7d0c7e1288acdd89c27c7076
Alexey Buslov <alex_aist@mail.ru>
2018-12-02 20:38:50 +0300

    add work with database

--- Source/app/src/main/java/com/example/alex_/project/AddCarActivity.java
+++ Source/app/src/main/java/com/example/alex_/project/AddCarActivity.java
@@ -1,8 +1,11 @@
 package com.example.alex_.project;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -17,18 +20,16 @@ public class AddCarActivity extends Activity {
     public String HowMuchOilStr;
     public String WhatMileageStr;
     public ArrayAdapter<String> adapter;
+    public SQLiteDatabase database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addcar);
-        String[] data = {"one", "two", "three", "four", "five"};
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,data);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         final TextView AddCarText = (TextView) findViewById(R.id.AddCarText);
         final TextView AddTankVolumeText = (TextView) findViewById(R.id.AddTankVolumeText);
         final TextView HowMuchOilText = (TextView) findViewById(R.id.HowMuchOilText);
         final TextView WhatMileageText = (TextView) findViewById(R.id.WhatMileageText);
-        final Spinner Cars = (Spinner) findViewById(R.id.Cars);
+        final MainActivity mainActivity = new MainActivity();
         Button SaveCarBtn = (Button) findViewById(R.id.SaveCarBtn);
         SaveCarBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,8 +38,21 @@ public class AddCarActivity extends Activity {
                 AddTankVolumeStr = AddTankVolumeText.getText().toString();
                 HowMuchOilStr =HowMuchOilText.getText().toString();
                 WhatMileageStr = WhatMileageText.getText().toString();
+                database = getBaseContext().openOrCreateDatabase("app.db", SQLiteDatabase.OPEN_READWRITE, null);
                 if (!AddCarStr.isEmpty() & !AddTankVolumeStr.isEmpty() & !HowMuchOilStr.isEmpty() & !WhatMileageStr.isEmpty()){
-                  //  Cars.setAdapter(adapter);
+                    int tankVol = Integer.parseInt(AddTankVolumeStr);
+                    int howMuchOil = Integer.parseInt(HowMuchOilStr);
+                    int whatMileage = Integer.parseInt(WhatMileageStr);
+                    ContentValues insertValues = new ContentValues();
+                       // database.execSQL("INSERT INTO car(name, tankvolume, mileage, intanknow) VALUES(" +
+                           //     AddCarStr + "," + tankVol + "," + howMuchOil + "," + whatMileage + ")");
+
+                    insertValues.put("name", AddCarStr);
+                    insertValues.put("tankvolume", tankVol);
+                    insertValues.put("mileage", whatMileage);
+                    insertValues.put("intanknow", howMuchOil);
+                    database.insert("car", null, insertValues);
+                    mainActivity.myList.add(AddCarStr);
                     Intent intent = new Intent(AddCarActivity.this, MainActivity.class);
                     startActivity(intent);
                 }
--- Source/app/src/main/java/com/example/alex_/project/MainActivity.java
+++ Source/app/src/main/java/com/example/alex_/project/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.alex_.project;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,9 +12,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import java.util.ArrayList;
+
+import static android.database.sqlite.SQLiteDatabase.OPEN_READWRITE;
+
 public class MainActivity extends AppCompatActivity {
+
+    public SQLiteDatabase db;
+    public ArrayAdapter<String> adapter;
+    public ArrayList<String> myList;
+    public MainActivity() {
+    }
+
+    public SQLiteDatabase getDb() {
+        return db;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +37,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.content_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Spinner Cars = (Spinner) findViewById(R.id.Cars);
+        myList = new ArrayList<String>();
 
+        db = getBaseContext().openOrCreateDatabase("app.db", OPEN_READWRITE , null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS car (id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                " name TEXT NOT NULL, tankvolume INTEGER NOT NULL, " +
+                "mileage INTEGER NOT NULL, lastmileage INTEGER, " +
+                "intanklast INTEGER, intanknow INTEGER NOT NULL)");
+        Cursor cursor = db.rawQuery("SELECT * FROM car;", null);
+        if(cursor.getCount() == 0){
+            Intent intent = new Intent(MainActivity.this, AddCarActivity.class);
+            startActivity(intent);
+        }
+        int count = cursor.getCount();
+        cursor.getString(1);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myList);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.addCarBtn);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,8 +63,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-        Spinner Cars = (Spinner) findViewById(R.id.Cars);
     }
 
     @Override
@@ -55,4 +86,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
